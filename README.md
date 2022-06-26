b6af36c4e920e7aacdeb27da489ad30c8ae1235f
mintain <3209304627@qq.com>
2022-06-27 00:06:38 +0800

    innit cluster

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 # 服务部署
 
 ## docker compose
-
+1. redis-sentinel
+2. redis-cluster
---
+++ docker-compose/redis-cluster/build.sh
@@ -0,0 +1,5 @@
+#!/binbash
+# apt-get install docker-compose-plugin
+chmod -R 0777 ./
+docker compose up -d
+# docker compose rm
